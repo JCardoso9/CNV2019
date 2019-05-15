60cd845d09d992095a88fe7565770575602a124d
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2019-05-15 16:44:01 +0100

    Update properties.

--- pt/ulisboa/tecnico/cnv/aws/autoscaler/EC2InstanceController.java
+++ pt/ulisboa/tecnico/cnv/aws/autoscaler/EC2InstanceController.java
@@ -1,44 +1,41 @@
 package pt.ulisboa.tecnico.cnv.aws.autoscaler;
 
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.model.*;
 import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 
-import java.util.ResourceBundle;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 
-
-
-
-public class EC2InstanceController{
-
+public class EC2InstanceController {
 
     private static ResourceBundle properties;
     private static String REGION = "region";
-    private static String AUTO_SCALER_AMI_ID = "auto_scaler_ami_id";
-    private static String AUTO_SCALER_NAME = "auto_scaler_name";
-    private static String LOAD_BALANCER_NAME = "load_balancer_name";
+    private static String INSTANCE_AMI_ID = "instance_ami_id";
     private static String INSTANCE_TYPE = "instance_type";
     private static String KEY_PAIR_NAME = "key_pair_name";
     private static String SECURITY_GROUP = "security_group";
-    private static String IDLE_TIMEOUT = "idle_timeout";
 
-    static String ec2InstanceID;
+    private String ec2InstanceID;
 
 
-    public EC2InstanceController(String ec2InstanceID) {
+    private EC2InstanceController(String ec2InstanceID) {
         this.ec2InstanceID = ec2InstanceID;
     }
 
-	public static EC2InstanceController requestNewEC2Instance(AmazonEC2 client) throws Exception {
+    public static EC2InstanceController requestNewEC2Instance(AmazonEC2 client) throws Exception {
 
         init();
+
         RunInstancesRequest runInstancesRequest =
                 new RunInstancesRequest();
 
         System.out.println("Creating EC2 instance... ");
-        runInstancesRequest.withImageId(properties.getString(AUTO_SCALER_AMI_ID))
+        runInstancesRequest
+                .withImageId(properties.getString(INSTANCE_AMI_ID))
                 .withInstanceType((properties.getString(INSTANCE_TYPE)))
                 .withMinCount(1)
                 .withMaxCount(1)
@@ -53,35 +50,16 @@ public class EC2InstanceController{
         System.out.println("EC2 instance created.");
 
 
-
         return new EC2InstanceController(instanceId);
     }
 
-
-
-
-    public static synchronized void shutDownEC2Instance(AmazonEC2 client) {
+    public synchronized void shutDownEC2Instance(AmazonEC2 client) {
         System.out.println("Shutting down instance with ID " + ec2InstanceID + "...");
         TerminateInstancesRequest termInstanceReq = new TerminateInstancesRequest();
         termInstanceReq.withInstanceIds(ec2InstanceID);
         client.terminateInstances(termInstanceReq);
         System.out.println("EC2 instance terminated. ");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     private static void init() throws Exception {
         try {
@@ -98,14 +76,12 @@ public class EC2InstanceController{
     private static void checkResourceBundleKeys(ResourceBundle props) throws Exception {
         // If the required properties exist and have a value
         if (!(props.containsKey(REGION)
-                && props.containsKey(AUTO_SCALER_AMI_ID)
-                && props.containsKey(AUTO_SCALER_NAME)
+                && props.containsKey(INSTANCE_AMI_ID)
                 && props.containsKey(INSTANCE_TYPE)
                 && props.containsKey(KEY_PAIR_NAME)
                 && props.containsKey(SECURITY_GROUP))
             || !(!props.getString(REGION).isEmpty()
-                && !props.getString(AUTO_SCALER_AMI_ID).isEmpty()
-                && !props.getString(AUTO_SCALER_NAME).isEmpty()
+                && !props.getString(INSTANCE_AMI_ID).isEmpty()
                 && !props.getString(INSTANCE_TYPE).isEmpty()
                 && !props.getString(KEY_PAIR_NAME).isEmpty()
                 && !props.getString(SECURITY_GROUP).isEmpty())) {
@@ -113,7 +89,4 @@ public class EC2InstanceController{
                     " Make sure all properties have been declared.");
         }
     }
-
-
-
 }
